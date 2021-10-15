9d38327447c238f549f845c80364e0e4b5a0ebb9
lysar1e <79013918+lysar1e@users.noreply.github.com>
2021-10-15 19:08:56 +0600

    Update _app.tsx

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,6 +2,8 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import NextNProgress from "nextjs-progressbar";
+import Script from "next/script";
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -28,6 +30,16 @@ function MyApp({ Component, pageProps }: AppProps) {
             height={5}
             showOnShallow={true}
         />
+        <Script strategy='lazyOnload' src={`https://www.googletagmanager.com/gtag/js?id=G-SV7L62HNPZ`} />
+        <Script id="google-analytics" strategy='lazyOnload'>
+            {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+            
+                gtag('config', 'G-SV7L62HNPZ');
+            `}
+        </Script>
       <Component {...pageProps} />
     </>
   );
